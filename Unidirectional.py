2e41b5c534f6ac99c070a2888a3d0a4dab7b8248
nouf1010 <nouf112211@gmail.com>
2024-03-30 01:46:19 +0400

    Added the person class to this file from the inheritance relationship to the unidirectional relationship.

--- Unidirectional.py
+++ Unidirectional.py
@@ -45,3 +45,38 @@ class Ticket:
 
     def get_price(self):
         return self.__price
+
+
+class Person:
+    '''Class to represent a person'''
+    # Constructor
+    def __init__(self, nameP, age, gender):
+        self._nameP = nameP
+        self._age = age
+        self._gender = gender
+        self._ticket = None  # Ticket attribute added to represent a person may possess a ticket
+
+    # Setters and getters
+    def set_nameP(self, nameP):
+        self._nameP = nameP
+
+    def get_nameP(self):
+        return self._nameP
+
+    def set_age(self, age):
+        self._age = age
+
+    def get_age(self):
+        return self._age
+
+    def set_gender(self, gender):
+        self._gender = gender
+
+    def get_gender(self):
+        return self._gender
+
+    def set_ticket(self, ticket):
+        self._ticket = ticket
+
+    def get_ticket(self):
+        return self._ticket
