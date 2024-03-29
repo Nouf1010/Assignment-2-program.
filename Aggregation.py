135afb1c67e113066d38c4ad3713fd011febfb44
nouf1010 <nouf112211@gmail.com>
2024-03-29 17:19:28 +0400

    Added the second class which is ExhibitionHall and wrote its attributes and setters and getters then wrote the str function to represent the Exhibition Hall object as a string.

--- Aggregation.py
+++ Aggregation.py
@@ -28,3 +28,43 @@ class Artwork:
     #Using the str function to represent the artwork object as a string
     def __str__(self):
         return "Artwork: " + self.__title + ", Artist: " + self.__artist + ", Style: " + self.__style
+
+class ExhibitionHall:
+    '''Class to represent an exhibition hall'''
+    # Constructor
+    def __init__(self, nameE, locationE, sizeE):
+        self.__nameE = nameE
+        self.__locationE = locationE
+        self.__sizeE = sizeE
+        self.__artworks = []  # List to hold Artwork objects
+
+    # Setters and getters
+    def set_name(self, nameE):
+        self.__nameE = nameE
+
+    def get_name(self):
+        return self.__nameE
+
+    def set_location(self, locationE):
+        self.__locationE = locationE
+
+    def get_location(self):
+        return self.__locationE
+
+    def set_size(self, sizeE):
+        self.__sizeE = sizeE
+
+    def get_size(self):
+        return self.__sizeE
+
+    def add_artwork(self, artwork):
+        self.__artworks.append(artwork)
+
+    #Using the str function to represent the Exhibition Hall object as a string
+    def __str__(self):
+        hall_info = "Exhibition Hall: " + self.__nameE + ", Location: " + self.__locationE + ", Size: " + self.__sizeE + "\n"
+        artwork_info = ""
+        # Using a for loop to iterate in the artworks and join their string representations
+        for artwork in self.__artworks:
+            artwork_info += str(artwork) + "\n" #using += to add then assign right away
+        return hall_info + artwork_info
