4b14d581ce1620d91eb4d2f59c16ef7650e42c9f
nouf1010 <nouf112211@gmail.com>
2024-03-29 15:37:32 +0400

    Added the str function for the ExhibitionHall class and wrote the second class which is Museum and added their attributes and setters and getters.

--- Composition.py
+++ Composition.py
@@ -24,3 +24,37 @@ class ExhibitionHall:
 
     def get_size(self):
         return self.__sizeE
+
+    #Using the str function to represent the ExhibitionHall object as a string
+    def __str__(self):
+        return "Exhibition Hall: " + self.__nameE + ", Location: " + self.__locationE + ", Size: " + self.__sizeE
+
+
+class Museum:
+    '''Class to represent a museum'''
+    # Constructor and making the attributes private
+    def __init__(self, nameM, locationM, sizeM):
+        self.__nameM = nameM
+        self.__locationM = locationM
+        self.__sizeM = sizeM
+        # Creating exhibition halls for the museum
+        self.__exhibition_halls = self.create_exhibition_halls()
+
+    # Setters and getters
+    def set_name(self, nameM):
+        self.__nameM = nameM
+
+    def get_name(self):
+        return self.__nameM
+
+    def set_location(self, locationM):
+        self.__locationM = locationM
+
+    def get_location(self):
+        return self.__locationM
+
+    def set_size(self, sizeM):
+        self.__sizeM = sizeM
+
+    def get_size(self):
+        return self.__sizeM
