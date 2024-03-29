8fbb6aca02734bfc5f0114bf67bcf214e2e92f12
Nouf1010 <159875164+Nouf1010@users.noreply.github.com>
2024-03-29 15:13:02 +0400

    Add files via upload

---
+++ Composition.py
@@ -0,0 +1,7 @@
+class ExhibitionHall:
+    '''Class to represent an exhibition hall'''
+    # Constructor and making the attributes private
+    def __init__(self, nameE, locationE, sizeE):
+        self.__nameE = nameE
+        self.__locationE = locationE
+        self.__sizeE = sizeE
