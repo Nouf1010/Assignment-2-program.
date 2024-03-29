5e77ab6f4f98d667926170fe51dd1f09ebe690d4
nouf1010 <nouf112211@gmail.com>
2024-03-29 15:17:35 +0400

    Added setters and getters

--- Composition.py
+++ Composition.py
@@ -5,3 +5,22 @@ class ExhibitionHall:
         self.__nameE = nameE
         self.__locationE = locationE
         self.__sizeE = sizeE
+
+    # Setters and getters
+    def set_name(self, nameE):
+        self.__nameE = nameE
+
+    def get_name(self):
+        return self.__nameE
+
+    def set_location(self, locationE):
+        self.__locationE = locationE
+
+    def get_location(self):
+        return self.__locationE
+
+    def set_size(self, sizeE):
+        self.__sizeE = sizeE
+
+    def get_size(self):
+        return self.__sizeE
